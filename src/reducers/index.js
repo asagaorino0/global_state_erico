6c5830c3d480d17bb667a17724127ed7fc4342e0
asagaorino0 <xcnsn724@yahoo.co.jp>
2021-04-11 14:44:12 +0900

    [add]08_20210410]

---
+++ src/reducers/index.js
@@ -0,0 +1,18 @@
+import { GET_DATA } from '../actions/index'
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case GET_DATA:
+
+            console.log(action.data)
+
+
+            return { ...state, user_data: action.data }
+
+
+        default:
+            return state
+    }
+}
+
+export default reducer
