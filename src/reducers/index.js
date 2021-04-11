bdfea93c71909f0fc5b03f5fd61595bebb83f22e
asagaorino0 <xcnsn724@yahoo.co.jp>
2021-04-11 17:53:21 +0900

    [add]08_NORMAL-1]

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,13 +3,19 @@ import { GET_DATA } from '../actions/index'
 const reducer = (state, action) => {
     switch (action.type) {
         case GET_DATA:
-
-            console.log(action.data)
-
-
-            return { ...state, user_data: action.data }
-
-
+            console.log(action.data)//atodekesu
+            //koremo,atodekesu
+            // function filterByID(item) {
+            //     if (item.id % 2 !== 0)
+            //         return true
+            // }
+            // let arrByID = arr.filter(filterByID)
+            const arr = action.data
+            const arrByID = arr.filter(function (item) {
+                return item.id % 2 !== 0;
+            })
+            console.log(arrByID)
+            return { ...state, user_data: arrByID }
         default:
             return state
     }
